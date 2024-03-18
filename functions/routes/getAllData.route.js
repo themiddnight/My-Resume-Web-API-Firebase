91c579c6a07ddf79918011d46c499c9063a9a345
Pathompong Thitithan <the.midnight.k.0173@gmail.com>
2024-03-18 21:04:35 +0700

    Refactor data retrieval in getAllData route

--- functions/routes/getAllData.route.js
+++ functions/routes/getAllData.route.js
@@ -4,17 +4,72 @@ const { db } = require('../db');
 const router = express.Router();
 
 async function getData() {
-  const profile = (await db.collection('profile').get()).docs[0].data();
-  const about = (await db.collection('about').get()).docs.map(doc => doc.data());
-  const experiences = (await db.collection('experiences').orderBy('to', 'desc').get()).docs.map(doc => doc.data());
-  const education = (await db.collection('education').orderBy('to', 'desc').get()).docs.map(doc => doc.data());
-  const personal_projects = (await db.collection('personal_projects').orderBy('createdAt', 'desc').get()).docs.map(doc => doc.data());
-  const skills = (await db.collection('skills').orderBy('order').get()).docs.map(doc => doc.data());
-  const tools = (await db.collection('tools').get()).docs.map(doc => doc.data());
-  const languages = (await db.collection('languages').get()).docs.map(doc => doc.data());
-  const certifications = (await db.collection('certifications').orderBy('issuedDate', 'desc').get()).docs.map(doc => doc.data());
-  const other_profiles = (await db.collection('other_profiles').get()).docs.map(doc => doc.data());
-  const public_notes = (await db.collection('public_notes').orderBy('createdAt', 'desc').limit(10).get()).docs.map(doc => doc.data());
+
+  const profile = (await db
+    .collection('profile')
+    .get()
+  ).docs[0].data();
+
+  const about = (await db
+    .collection('about')
+    .orderBy('order')
+    .get()
+  ).docs.map(doc => doc.data());
+
+  const experiences = (await db
+    .collection('experiences')
+    .orderBy('to', 'desc')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+    const education = (await db
+    .collection('education')
+    .orderBy('to', 'desc')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const personal_projects = (await db
+    .collection('personal_projects')
+    .orderBy('createdAt', 'desc')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const skills = (await db
+    .collection('skills')
+    .orderBy('order')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const tools = (await db
+    .collection('tools')
+    .orderBy('title')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const languages = (await db
+    .collection('languages')
+    .orderBy('order')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const certifications = (await db
+    .collection('certifications')
+    .orderBy('issuedDate', 'desc')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const other_profiles = (await db
+    .collection('other_profiles')
+    .orderBy('order')
+    .get()
+  ).docs.map(doc => doc.data());
+  
+  const public_notes = (await db
+    .collection('public_notes')
+    .orderBy('createdAt', 'desc')
+    .limit(10)
+    .get()
+  ).docs.map(doc => doc.data());
 
   return {
       profile,
